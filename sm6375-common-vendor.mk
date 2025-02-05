4a9d1937decc13b8f0f0c0149d8682d06e04aaa7
Sugakesshaa <sugakesshaa@pixelos.net>
2025-02-06 04:17:40 +0800

    kk
    Change-Id: I96e35aa069df047dc0cdc3a6079554cf5e13d92a

--- sm6375-common-vendor.mk
+++ sm6375-common-vendor.mk
@@ -854,14 +854,14 @@ PRODUCT_PACKAGES += \
     vendor.oplus.hardware.performance-V1-ndk_platform \
     vendor.oplus.hardware.stability.oplus_project-V1-ndk_platform \
     vendor.oplus.hardware.stability.oplus_project@1.0 \
-    libcodec2_hidl_plugin_odm \
-    libcodec2_hidl_sp@1.0_odm \
+    libcodec2_hidl@1.0_sp \
+    libcodec2_hidl_plugin_sp \
     libcodec2_soft_ddpdec_sp \
     libcodec2_store_dolby_sp \
-    libcodec2_vndk_sp_odm \
+    libcodec2_vndk_sp \
     liboemcrypto \
     liboplus_service \
-    libui_sp_odm \
+    libui_sp \
     libwvhidl \
     libwvdrmengine \
     vendor.oplus.hardware.appradio@1.0 \
